4bc9dd8fe7242d37493cf544c348869eee96efcd
pavel11123 <korobko.11@inbox.ru>
2023-01-23 22:37:02 +0600

    add lazy load

--- main/js/script.js
+++ main/js/script.js
@@ -3,6 +3,34 @@ window.onload = function () {
   preloader.style.display = "none";
 };
 
+//lazy load
+const images = document.querySelectorAll("img");
+const optionsLazyLoad = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+function handleImg(myImg, observerLazyLoad) {
+  myImg.forEach((myImgSingle) => {
+    console.log(myImgSingle.intersectionRatio);
+    if (myImgSingle.intersectionRatio > 0) {
+      loadImage(myImgSingle.target);
+    }
+  });
+}
+
+function loadImage(image) {
+  image.src = image.getAttribute("data");
+}
+
+const observerLazyLoad = new IntersectionObserver(handleImg, optionsLazyLoad);
+
+images.forEach((img) => {
+  observerLazyLoad.observe(img);
+});
+//lazy load
+
 const slider = () => {
   $(document).ready(function () {
     $(".slider").slick({
