8309eaafbbc6bb95fa641771ed42912de954126d
pavel11123 <korobko.11@inbox.ru>
2023-01-23 22:41:14 +0600

    fix lazy load

--- main/js/script.js
+++ main/js/script.js
@@ -1,35 +1,38 @@
+//preloader
 window.onload = function () {
   let preloader = document.querySelector(".preloader");
   preloader.style.display = "none";
 };
 
 //lazy load
-const images = document.querySelectorAll("img");
-const optionsLazyLoad = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.1,
-};
+const lazyLoadImage = () => {
+  const images = document.querySelectorAll("img");
+  const optionsLazyLoad = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
 
-function handleImg(myImg, observerLazyLoad) {
-  myImg.forEach((myImgSingle) => {
-    console.log(myImgSingle.intersectionRatio);
-    if (myImgSingle.intersectionRatio > 0) {
-      loadImage(myImgSingle.target);
-    }
+  function handleImg(myImg, observerLazyLoad) {
+    myImg.forEach((myImgSingle) => {
+      // console.log(myImgSingle.intersectionRatio);
+      if (myImgSingle.intersectionRatio > 0) {
+        loadImage(myImgSingle.target);
+      }
+    });
+  }
+
+  function loadImage(image) {
+    image.src = image.getAttribute("data");
+  }
+
+  const observerLazyLoad = new IntersectionObserver(handleImg, optionsLazyLoad);
+
+  images.forEach((img) => {
+    observerLazyLoad.observe(img);
   });
-}
-
-function loadImage(image) {
-  image.src = image.getAttribute("data");
-}
-
-const observerLazyLoad = new IntersectionObserver(handleImg, optionsLazyLoad);
-
-images.forEach((img) => {
-  observerLazyLoad.observe(img);
-});
-//lazy load
+};
+lazyLoadImage();
 
 const slider = () => {
   $(document).ready(function () {
